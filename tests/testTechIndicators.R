8a44f0f6acb9e94665b998ed9f95242a5289dccb
Michael Szczepaniak <mszczepaniak@pobox.com>
2016-05-14 20:47:56 -0600

    testTechIndicators.R - impl test.calcEma

--- tests/testTechIndicators.R
+++ tests/testTechIndicators.R
@@ -15,5 +15,14 @@ test.calcSma <- function() {
 }
 
 test.calcEma <- function() {
-    
+    period <- 10
+    prices <- c(22.27, 22.19, 22.08, 22.17, 22.18,
+                22.13, 22.23, 22.43, 22.24, 22.29,
+                22.15, 22.39)
+    targEma10 <- c(NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN,
+                   mean(prices[1:10]), 22.2081, 22.2412)
+    currEma10 <- calcEma(prices, period)
+    for(i in 1:length(prices)) {
+        checkEqualsNumeric(targEma10[i], currEma10[i], tolerance=0.001)
+    }
 }
