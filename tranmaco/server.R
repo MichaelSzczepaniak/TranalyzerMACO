d884daefbeb52a7b328639e83287d8a033941f2d
Michael Szczepaniak <mszczepaniak@pobox.com>
2016-05-18 19:01:45 -0600

    server.R - add pre-click of 'Get Quote Data' button message, add quote data acquisition

--- tranmaco/server.R
+++ tranmaco/server.R
@@ -1,15 +1,33 @@
+source("DataManager.R")
 source("TradingEvaluator.R")
 source("StrategySimulator.R")
 
 shinyServer(
     function(input, output) {
+        
         output$outQuoteDataStatus <- eventReactive(input$inQueryQuotes, {
-            quoteDateRange <- sprintf('%s%s%s%s', 'from ',
-                                      input$inQueryDateRange[1], ' to ',
-                                      input$inQueryDateRange[2])
-            sprintf('%s%s%s%s', input$ticker, ' quotes ', quoteDateRange,
-                    ' acquired.')
-        })
+            
+            if(input$inQueryQuotes > 0) {
+                pdat <- getStockQuotes(input$ticker,
+                                       input$inQueryDateRange[1],
+                                       input$inQueryDateRange[2])
+                quoteDateRange <- sprintf('%s%s%s%s', 'from ',
+                                          input$inQueryDateRange[1], ' to ',
+                                          input$inQueryDateRange[2])
+                if(nrow(pdat) > 0) {
+                    sprintf('%s%s%s%s', input$ticker, ' quotes ', quoteDateRange,
+                            ' acquired.')
+                } else {
+                    sprintf('%s%s%s%s', input$ticker, ' quotes ', quoteDateRange,
+                            ' NOT acquired.')
+                }
+                
+                
+            } else {
+                sprintf('%s', 'NO QUOTE DATA LOADED: Get Quote data before beginning.')
+            }
+            
+        }, ignoreNULL = FALSE)
         
         
     }
