a835a26de5d7926730dfcf64e0ba4fca85c33b56
Michael Szczepaniak <mszczepaniak@pobox.com>
2016-05-20 14:42:08 -0600

    server.R - convert dates to strings in output$outQuoteDataStatus so they can be handled properly downstream by DataManager.R functions

--- tranmaco/server.R
+++ tranmaco/server.R
@@ -14,31 +14,31 @@ shinyServer(
             }
             maToken <- sprintf('%s%s%s%s%s%s', maToken, '(', input$inFastSlowMavg[1],
                                ',', input$inFastSlowMavg[2], ')')
-            
-            
+
+
             sabToken <- sprintf('%s%d', 'Start Bal=$', 10000)
             if(input$inAccBalance != 10000) {
                 sabToken <- sprintf('%s%d', 'Start Bal=$', input$inAccBalance)
             }
             pmToken <- sprintf('%s%s', 'PM=', input$inPosMgmt)
-            
+
             params <- sprintf('%s%s%s%s%s', maToken, ' | ',
                                             sabToken, ' | ', pmToken)
             params
         }
+
         output$outSimParams <- renderText({
             simConfig()
         })
         
         output$outQuoteDataStatus <- eventReactive(input$inQueryQuotes, {
-            
+            # http://stackoverflow.com/questions/33662033/shiny-how-to-make-reactive-value-initialize-with-default-value
             if(input$inQueryQuotes > 0) {
-                pdat <- getStockQuotes(input$ticker,
-                                       input$inQueryDateRange[1],
-                                       input$inQueryDateRange[2])
+                startDateStr <- as.character(input$inQueryDateRange[1])
+                endDateStr <- as.character(input$inQueryDateRange[2])
+                pdat <- getStockQuotes(input$ticker, startDateStr, endDateStr)
                 quoteDateRange <- sprintf('%s%s%s%s', 'from ',
-                                          input$inQueryDateRange[1], ' to ',
-                                          input$inQueryDateRange[2])
+                                          startDateStr, ' to ', endDateStr)
                 if(nrow(pdat) > 0) {
                     sprintf('%s%s%s%s', input$ticker, ' quotes ', quoteDateRange,
                             ' acquired.')
