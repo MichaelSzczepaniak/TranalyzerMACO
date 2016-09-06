359e36011c8931659ed1ac0abb60ffa1ea90f646
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-09-06 13:07:06 -0600

    ui.R - remove selected line so first field will render, move get quotes action button above the moving avg radio buttons, change some of the variable names to use Python style

--- tranmaco/ui.R
+++ tranmaco/ui.R
@@ -1,15 +1,15 @@
-
-configUrl <- './data/nonquotes/config.csv'
-config <- read.csv(configUrl)
-signal.tab.content.file <- config[config$param == 'signal.tab.content',]$value
-source.tab.content.file <- config[config$param == 'source.tab.content',]$value
-config.dir <- config[config$param == 'config.dir',]$value
-signal.tab.content.file <- paste0(config.dir, signal.tab.content.file)
-source.tab.content.file <- paste0(config.dir, source.tab.content.file)
-signal.tab.content <- readChar(signal.tab.content.file,
-                               file.info(signal.tab.content.file)$size)
-source.tab.content <- readChar(source.tab.content.file,
-                               file.info(source.tab.content.file)$size)
+## load configuration info
+config_url <- './data/nonquotes/config.csv'
+config <- read.csv(config_url, stringsAsFactors=FALSE)
+signal_tab_content_file <- config[config$param == 'signal_tab_content',]$value
+source_tab_content_file <- config[config$param == 'source_tab_content',]$value
+config_dir <- config[config$param == 'config_dir',]$value
+signal_tab_content_file <- paste0(config_dir, signal_tab_content_file)
+source_tab_content_file <- paste0(config_dir, source_tab_content_file)
+signal_tab_content <- readChar(signal_tab_content_file,
+                               file.info(signal_tab_content_file)$size)
+source_tab_content <- readChar(source_tab_content_file,
+                               file.info(source_tab_content_file)$size)
 
 makeOptList <- function(labels, values) {
     names(values) <- labels
@@ -25,50 +25,45 @@ tenYearsAgoToday <- function() {
 }
 
 # Default dates to use with live data
-simEndDate <- as.character(Sys.Date())
-simStartDate <- as.character(Sys.Date() - 365)
-simStartDateMin = tenYearsAgoToday(); simEndDateMax = simEndDate
-
-# Get company drop down values
-companyDataUrl <- "./data"
-demoQuoteDataFiles <- dir('./data', '*.csv')
-demoTickersList <- strsplit(demoQuoteDataFiles, '.csv')
+end_date_default <- as.character(Sys.Date() - 1)
+start_date_default <- as.character(Sys.Date() - 365)
+earliest_start_date = tenYearsAgoToday()
+latest_end_date = end_date_default
 
 # Get moving average options for radio buttons
-movingAvgUrl <- "./data/nonquotes/moving_avgs.csv"
-maOptions <- read.csv(movingAvgUrl, stringsAsFactors = FALSE)
-maOptionsList <- makeOptList(maOptions$MA_Type, maOptions$option)
+moving_avg_url <- "./data/nonquotes/moving_avgs.csv"
+ma_options <- read.csv(moving_avg_url, stringsAsFactors = FALSE)
+ma_options_list <- makeOptList(ma_options$MA_Type, ma_options$option)
 
 # Get postion mgmt strategies to populate Position Management select list
-posMgmtStratsUrl <- "./data/nonquotes/positionStrats.csv"
-pmOptions <- read.csv(posMgmtStratsUrl, stringsAsFactors = FALSE)
-# pmOptionsList <- as.list((pmOptions$Position_Sizing_Strategy))
+pos_mgmt_strats_url <- "./data/nonquotes/positionStrats.csv"
+pm_options <- read.csv(pos_mgmt_strats_url, stringsAsFactors = FALSE)
 # only first position mgmt option is implemented
-pmOptionsList <- makeOptList(pmOptions$PSS_Type[1], pmOptions$option[1])
+pm_options_list <- makeOptList(pm_options$PSS_Type[1], pm_options$option[1])
 
 fluidPage(
     headerPanel("MACO Analyzer"),
     sidebarPanel(
         # http://shiny.rstudio.com/gallery/selectize-examples.html example #6
-        textInput('inTicker', label=h4("Company:")),
+        textInput('inTicker', label=h4("Ticker:")),
         dateRangeInput('inQueryDateRange', label = h4("Quotes Date Range:"),
-                       start=simStartDate, end=simEndDate,
-                       min=simStartDateMin, max=simEndDateMax),
+                       start=start_date_default, end=end_date_default,
+                       min=earliest_start_date, max=latest_end_date),
+        actionButton('inQueryQuotes', 'Get Quote Data'),
         radioButtons('inMovAvg', label=h4("Moving Average (MA):"),
-                     choices=maOptionsList, selected=1, inline = TRUE),
-        # actionButton('inQueryQuotes', 'Get Quote Data'),
+                     choices=ma_options_list, selected=1, inline = TRUE),
         sliderInput('inFastSlowMavg', h4("Fast & Slow MA Days"),
                     min = 2, max = 200, value = c(9,18)),
         numericInput('inAccBalance', 'Starting Account Balance:',
                      10000, min = 5000, max = 1000000, step = 500),
         selectInput('inPosMgmt', label=h4("Position Management:"),
-                    choices=pmOptionsList, selected=1),
+                    choices=pm_options_list, selected=1),
         actionButton('inRunSimButton', 'Run Simulation')
 
     ),
     mainPanel(
         tabsetPanel(
-            tabPanel('Analyze',
+            tabPanel("Analyzer",
                 h4('Quote Data Status:'),
                 verbatimTextOutput("outQuoteDataStatus"),
                 h4('Simulation Parameters:'),
@@ -91,11 +86,10 @@ fluidPage(
                      h4("Breakdown of Simulated Trade Results:"),
                      plotOutput("oidTradesResultsHist")
             ),
-            tabPanel('User Guide', HTML(signal.tab.content)
+            tabPanel('User Guide', HTML(signal_tab_content)
             ),
-            tabPanel('Source Code', HTML(source.tab.content)
-            ),
-            selected = 'Analyze'
+            tabPanel('Source Code', HTML(source_tab_content)
+            )
         )
     )
 )
